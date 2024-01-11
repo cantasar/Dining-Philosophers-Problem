5038adf3f2e036e5f75b775638688a2940fbebfd
cantasar <62062865+cantasar@users.noreply.github.com>
2024-01-11 20:59:00 +0300

    Add files via upload

---
+++ Philosophers-main/philosophers.h
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctasar <ctasar@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/25 13:39:45 by ctasar            #+#    #+#             */
+/*   Updated: 2024/01/11 16:59:20 by ctasar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+typedef unsigned long long	t_time;
+
+typedef struct s_philo
+{
+	int				index;
+	int				number_philo;
+	t_time			time_to_die;
+	t_time			time_to_sleep;
+	t_time			time_to_eat;
+	int				must_eat;
+
+	t_time			start_time;
+	t_time			last_eat_time;
+
+	pthread_t		thread;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	pthread_mutex_t	lock;
+	pthread_mutex_t	dead_lock;
+	pthread_mutex_t	meal_lock;
+
+}	t_philo;
+
+//init 
+int		check_args(int argc, char **argv);
+void	init_args(int argc, char **argv, t_philo *philo);
+void	init_forks(t_philo *philo, pthread_mutex_t *forks);
+void	create_threads(t_philo *philo);
+
+//utils
+int		ft_atoi(char *str);
+t_time	get_time(void);
+void	ft_sleep(int wait_time);
+
+//life
+void	*lifecycle(void *arg_philo);
+
+#endif
---
+++ Philosophers-main/srcs/init.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctasar <ctasar@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/17 14:22:33 by ctasar            #+#    #+#             */
+/*   Updated: 2024/01/11 17:54:04 by ctasar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../philosophers.h"
+
+int	check_args(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (argc == 5 || argc == 6)
+	{
+		i = 1;
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+					return (0);
+				if (ft_atoi(argv[i]) > 2147483647 || ft_atoi(argv[i]) == 0)
+					return (0);
+				j++;
+			}
+			i++;
+		}
+	}
+	else
+		return (0);
+	return (1);
+}
+
+void	init_args(int argc, char **argv, t_philo *philo)
+{
+	int	i;
+
+	i = 0;
+	while (i < ft_atoi(argv[1]))
+	{
+		philo[i].index = i;
+		philo[i].number_philo = ft_atoi(argv[1]);
+		philo[i].time_to_die = ft_atoi(argv[2]);
+		philo[i].time_to_eat = ft_atoi(argv[3]);
+		philo[i].time_to_sleep = ft_atoi(argv[4]);
+		if (argc == 6)
+			philo[i].must_eat = ft_atoi(argv[5]);
+		else
+			philo[i].must_eat = -1;
+		philo[i].start_time = get_time();
+		philo[i].last_eat_time = get_time();
+		i++;
+	}
+}
+
+void	init_forks(t_philo *philo, pthread_mutex_t *forks)
+{
+	int	i;
+
+	i = 0;
+	while (i < philo->number_philo)
+	{
+		philo[i].left_fork = &forks[i];
+		philo[i].right_fork = &forks[(i + 1) % philo->number_philo];
+		i++;
+	}
+	i = 0;
+	while (i < philo->number_philo)
+	{
+		pthread_mutex_init(philo[i].left_fork, NULL);
+		pthread_mutex_init(philo[i].right_fork, NULL);
+		pthread_mutex_init(&philo[i].lock, NULL);
+		i++;
+	}
+}
+
+void	create_threads(t_philo *philo)
+{
+	int	i;
+
+	i = 0;
+	while (i < philo->number_philo)
+	{
+		pthread_create(&philo[i].thread, NULL, &lifecycle, &philo[i]);
+		i++;
+		usleep(100);
+	}
+	while (1)
+	{
+		
+	}
+	
+	while (i < philo->number_philo)
+	{
+		pthread_join(philo->thread, NULL);
+		i++;
+	}
+
+}
